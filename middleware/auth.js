1b8e23b35927dc3ec1890b190961c5ec3a7d4382
ve0manda <amm141172@gmail.com>
2026-07-21 03:01:05 +0300

    Add premium expiration check in authentication middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,17 @@
 const db = require('../db/database');
 
+function expirePremiumIfNeeded(userId) {
+  db.prepare(`
+    UPDATE users SET is_premium = 0
+    WHERE id = ? AND is_premium = 1 AND premium_expires_at IS NOT NULL AND premium_expires_at < datetime('now')
+  `).run(userId);
+}
+
 function requireAuth(req, res, next) {
   if (!req.session.userId) {
     return res.redirect('/login');
   }
+  expirePremiumIfNeeded(req.session.userId);
   next();
 }
 
